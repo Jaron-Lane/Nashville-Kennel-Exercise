75cf26837cb4d160b978cbde10ee2ffa54c4e46f
Jaron-Lane <jaronwohlwend@icloud.com>
2020-12-01 11:56:15 -0600

    Displaying full name with animals

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -3,6 +3,7 @@ import { AnimalContext } from "./AnimalProvider"
 import { LocationContext } from "../location/LocationProvider"
 import { CustomerContext } from "../customer/CustomerProvider"
 import { Animal } from "./Animal"
+import "./Animal.css"
 
 
 export const AnimalsList = () => {
@@ -12,23 +13,27 @@ export const AnimalsList = () => {
     const { customers, getCustomers } = useContext(CustomerContext)
 
     /*
-        What's the effect this is reponding to? Component was
-        "mounted" to the DOM. React renders blank HTML first,
+        Component was "mounted" to the DOM. React renders blank HTML first,
         then gets the data, then re-renders.
     */
     useEffect(() => {
         console.log("AnimalList: Initial render before data")
-        getAnimals()
+        getLocations()
         .then(getCustomers)
-        .then(getLocations)
+        .then(getAnimals)
     }, [])
 
-   
-    return (
-        <div className="animals">
-        {
-            animals.map(ani => <Animal key={ani.id} animal={ani} />)
-        }
+   return ( 
+       <div className="animals"> 
+            {animals.map(animal => {
+                const owner = customers.find(c => c.id === animal.customerId)
+                const clinic = locations.find(l => l.id === animal.locationId)
+
+                return <Animal key={animal.id}
+                            location={clinic}
+                            customer={owner}
+                            animal={animal} />
+            })
+            }
         </div>
-    )
-}
+   )}
